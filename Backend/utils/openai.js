90769d3b8f26d8755da33ee38558bf49ce2158ae
Samanyu 3482 <gautamsamanyu3482@gmail.com>
2026-09-13 14:32:11 +0530

    added openAI API route

---
+++ Backend/utils/openai.js
@@ -0,0 +1,45 @@
+import 'dotenv/config';
+
+const getOpenAIAPIResponse = async (message) => {
+    try {
+
+        const options = {
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${process.env.ASI_API_KEY}`
+            },
+
+            body: JSON.stringify({
+                model: "asi1",
+                messages: [
+                    {
+                        role: "assistant",
+                        content: req.body.message
+                    }
+                ]
+            })
+        };
+
+        const response = await fetch(
+            "https://inference.asicloud.cudos.org/v1/chat/completions", options
+
+        );
+
+        const data = await response.json();
+
+
+
+        return (data.choices[0].message.content);
+
+    } catch (error) {
+
+        console.error(error);
+
+
+    };
+
+}
+
+export default getOpenAIAPIResponse;
